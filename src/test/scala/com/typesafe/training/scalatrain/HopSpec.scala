35d71fcd4bbc08d35677b44c61c91848474c7d89
Alex Yakubovich <alex@paytmlabss-MacBook-Pro.local>
2015-09-16 17:54:11 -0400

    clean up test

--- src/test/scala/com/typesafe/training/scalatrain/HopSpec.scala
+++ src/test/scala/com/typesafe/training/scalatrain/HopSpec.scala
@@ -8,13 +8,6 @@ import TestData._
 import java.lang.{ IllegalArgumentException => IAE }
 class HopSpec extends WordSpec with Matchers {
 
-  /*"Creating a Hop" should {
-    "throw an IllegalArgumentException if not all three arguments are specified" in {
-      an[IAE] should be thrownBy Hop(frankfurt, ice726)
-      an[IAE] should be thrownBy Hop(frankfurt, cologne)
-    }
-  }
-  */
   "Creating a Hop" should {
     "throw an IllegalArgumentException if the endpoints are not on its train's schedule" in {
       an[IAE] should be thrownBy Hop(frankfurt, cologne, ice726)
@@ -28,5 +21,3 @@ class HopSpec extends WordSpec with Matchers {
   }
 
 }
-
-import org.scalatest.{ Matchers, WordSpec }
