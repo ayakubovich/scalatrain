08d1e6eed5f7411bc989fe32e26923b17e38ff0a
George <movingapple@gmail.com>
2015-09-17 08:50:06 -0400

    schedule time should be sorted

--- src/main/scala/com/typesafe/training/scalatrain/Train.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Train.scala
@@ -2,26 +2,21 @@ package com.typesafe.training.scalatrain
 
 case class Train(info:TrainInfo, schedule:Seq[(Time, Station)]) {
   require(schedule.length > 1, "Required schedule to be of sequence of length > 2, got a sequence of length " + schedule.length)
-  //TODO: verify schedule is strictly increasing in time
+
+  val scheduleSorted = {
+    val times = schedule.map(_._1)
+    times.size == 0 || times.zip(times.tail).forall{case (prev, next) => prev < next}
+  }
+
+  require(scheduleSorted)
 
   val stations: Seq[Station] = schedule.map(_._2)
 
   def timeAt(station: Station): Option[Time] =
     schedule.find(stop => stop._2 == station).map(found => found._1)
 
-  def genHop(from:Station, to: Station, departure: Time): Seq[Hop] = {
-    /*
-    val fromIx = this.schedule.indexWhere(s => s._2 == from & s._1 > departure)
-    val toIx = this.schedule.indexWhere(_._2 == to)
-    if (fromIx != -1){
-      hops.slice(fromIx, toIx)
-    } else{
-      Seq.empty[Hop]
-    }
-  }
-  */
+  def genHop(from:Station, to: Station, departure: Time): Seq[Hop] =
     hops.dropWhile(hop => !(hop.from == from && hop.departure > departure)).reverse.dropWhile(_.to != to).reverse
-  }
 
 
   def backToBack: Seq[(Station, Station)] = {
