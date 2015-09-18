5a7116a99d512abd4112cf6b44d0c648a3d54816
George <movingapple@gmail.com>
2015-09-18 11:21:34 -0400

    train info keeps last maintained date

--- src/main/scala/com/typesafe/training/scalatrain/TrainInfo.scala
+++ src/main/scala/com/typesafe/training/scalatrain/TrainInfo.scala
@@ -1,9 +1,12 @@
 package com.typesafe.training.scalatrain
 
+import com.github.nscala_time.time.Imports._
+
 abstract class TrainInfo {
   def number: Int
+  def lastMaintained: Option[DateTime]
 }
 
-case class InterCityExpress(number:Int) extends TrainInfo
-case class RegionalExpress(number:Int, hasWifi:Boolean = false) extends TrainInfo
-case class BavarianRegional(number:Int) extends TrainInfo
+case class InterCityExpress(number:Int, lastMaintained:Option[DateTime] = None) extends TrainInfo
+case class RegionalExpress(number:Int, hasWifi:Boolean = false, lastMaintained:Option[DateTime] = None) extends TrainInfo
+case class BavarianRegional(number:Int, lastMaintained:Option[DateTime] = None) extends TrainInfo
