ee70235b6913c8832bca806a7d35aced4642bf26
Alex Yakubovich <alex.yakubovich@gmail.com>
2015-09-18 10:51:32 -0400

    define method to find days of travel between 2 dates

--- src/main/scala/com/typesafe/training/scalatrain/Schedule.scala
+++ src/main/scala/com/typesafe/training/scalatrain/Schedule.scala
@@ -1,6 +1,6 @@
 package com.typesafe.training.scalatrain
 import com.github.nscala_time.time.Imports._
-
+import org.joda.time.Days
 case class Schedule (schedule:Seq[(Time, Station, Double)], recurring: Set[Int] = Set.empty, exceptionDates:Set[DateTime] = Set.empty){
 
   require(schedule.length > 1, "Required schedule to be of sequence of length > 2, got a sequence of length " + schedule.length)
@@ -19,4 +19,18 @@ case class Schedule (schedule:Seq[(Time, Station, Double)], recurring: Set[Int] 
   def timeAt(station: Station): Option[Time] =
     schedule.find(stop => stop._2 == station).map(found => found._1)
 
+  def numDaysTravelledBetween2Dates(date1:DateTime, date2:DateTime):Int= {
+    val week = Set(1,2,3,4,5,6,7)
+    val partialWeek1 = week.filter(_>=date1.getDayOfWeek)
+    val partialWeek2 = week.filter(_<=date2.getDayOfWeek)
+
+    val numFullWeeks= Days.daysBetween(date1,date2).getDays/7
+
+    val exceptionDatesBetween2Dates = exceptionDates.toSeq.filter(x => x>=date1 && x <= date2).length
+
+    recurring.intersect(partialWeek1).size +
+      recurring.intersect(partialWeek2).size+
+      recurring.size * numFullWeeks -
+      exceptionDatesBetween2Dates
+  }
 }
